d297bd433b2133f0ff44b73e67d48f67e1eea231
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-08-10 18:11:25 +0300

    Update 3

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,1 +1,27 @@
 from rest_framework import serializers
+from core.models import Game, GameComment, ContactForm, Career, CareerForm, Founder, SocialMedia, SiteSettings
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ("name", "gametype", "content", "logo")
+
+class GameRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = "__all__"
+
+class GameCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameComment
+        fields = "__all__"
+
+class ContactFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactForm
+        fields = "__all__"
+
+class CareerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Career
+        fields = "__all__"
