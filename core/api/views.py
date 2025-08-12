08bc5970cd8a3dd8ccb3a636ccebc0593eb97801
Mehdiyeva9 <nazakatmehdiyeva9@gmail.com>
2025-08-12 20:23:55 +0300

    Update 5

--- core/api/views.py
+++ core/api/views.py
@@ -1,1 +1,27 @@
 from core import views
+from core.models import Game, GameComment, ContactForm, Career, CareerForm, Founder, SocialMedia, SiteSettings
+from core.api.serializers import (
+    GameSerializer, GameRetrieveSerializer, GameCommentSerializer, ContactFormSerializer, CareerSerializer,
+    CareerFormSerializer, FounderSerializer, SocialMediaSerializer, SiteSettingsSerializer
+)
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+
+class GameListAPIView(ListAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class GameRetrieveAPIView(RetrieveAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameRetrieveSerializer
+
+class GameCommentListAPIView(ListAPIView):
+    queryset = GameComment.objects.all()
+    serializer_class = GameCommentSerializer
+
+class ContactFormCreateAPIView(CreateAPIView):
+    queryset = ContactForm.objects.all()
+    serializer_class = ContactFormSerializer
+
+class CareerListAPIView(ListAPIView):
+    queryset = Career.objects.all()
+    serializer_class = CareerSerializer
